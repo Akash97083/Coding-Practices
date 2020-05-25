8faa96be92a02222fd13ca8c2c199e526ebf0cf6
Sunny Yadav <sunny.yadav@graniteshares.com>
2020-05-26 00:40:59 +0530

    Leet code mock interview

---
+++ LeetCode/RandomeQuestion/HammingDistance.cpp
@@ -0,0 +1,38 @@
+// Hamming distance
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+
+*/
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        
+        int hDistance = 0;
+        
+        for(int i = 0; i < 31; i++)
+        {
+            hDistance += (((y & (1 << i)) == 0) ^ ((x & (1 << i)) == 0) );
+            
+        }
+        return hDistance;
+    }
+};
